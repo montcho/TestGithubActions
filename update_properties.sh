50508ff2348556faec2c621e54c6db9fd7705ad9
Juan David Montes Vasco <jmontes@cvent.com>
2022-06-03 11:05:37 -0500

    Change Properties

--- update_properties.sh
+++ update_properties.sh
@@ -9,7 +9,7 @@ if [ -e "$filename" ]; then
   sed -i "s/.*DESC.*/DESC=$3/" $filename
 else
   touch local.properties
-  echo "VERSION_CODE=\"$1\"" >> local.properties
-  echo "VERSION_NAME=\"$2\"" >> local.properties
+  echo "VERSION_CODE=$1" >> local.properties
+  echo "VERSION_NAME=$2" >> local.properties
   echo "DESC=$3" >> local.properties
 fi
